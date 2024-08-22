6c8c57e6074bceae02ad9b3f5d4daea4df4469f1
Alvaro Villalobos Vela <vivealed@gmail.com>
2024-08-22 12:19:40 -0400

    update api.js, publicacion.js and usuario.js

--- api.js
+++ api.js
@@ -18,7 +18,7 @@ app.use(require("./routes/usuario.js"));
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 5050;
 
 // Iniciar el servidor en el puerto definido
 app.listen(port, () => {
